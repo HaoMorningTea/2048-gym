815219596d097ea3e4f01903a3725655ca0dbd3b
Chongshan0Lin <chongshan0lin@gmail.com>
2024-04-24 17:19:10 -0400

    Changed obs space and action space in  init in customenv

--- gym_game/envs/custom_env.py
+++ gym_game/envs/custom_env.py
@@ -4,11 +4,21 @@ import numpy as np
 from gym_game.envs.pygame_2d import PyGame2D
 
 class CustomEnv(gym.Env):
-    #metadata = {'render.modes' : ['human']}
+    # metadata = {'render.modes' : ['human']}
+    # How do we set up environment here?
     def __init__(self):
+        # The game seems correct..?
         self.pygame = PyGame2D()
-        self.action_space = spaces.Discrete(3)
-        self.observation_space = spaces.Box(np.array([0, 0, 0, 0, 0]), np.array([10, 10, 10, 10, 10]), dtype=np.int)
+        # It should be 4 because there are four actions that can be performed
+        # self.action_space = spaces.Discrete(3)
+        self.action_space = spaces.Discrete(4)
+
+        ### Observation space is a 4x4 board
+
+        # self.observation_space = spaces.Box(np.array([0, 0, 0, 0, 0]), np.array([10, 10, 10, 10, 10]), dtype=np.int)
+        # self.observation_space = spaces.Box(np.array([[0, 0, 0, 0],[0, 0, 0, 0],[0, 0, 0, 0],[0, 0, 0, 0]]), dtype=np.int)
+        self.observation_space = spaces.Box(low=0, high=255, shape=(4, 4), dtype=np.int)
+
 
     def reset(self):
         del self.pygame
