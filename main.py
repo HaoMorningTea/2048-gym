5fda9c6c0c0328a6378cfcaac17bea2109290d5d
Hao Gu <haogu2003@gmail.com>
2024-05-01 17:00:43 -0400

    modified q-table structure

--- main.py
+++ main.py
@@ -4,9 +4,27 @@ import math
 import random
 import gym
 import gym_game
+import pickle
+
+def save_q_table(q_table, filename):
+    with open(filename, "wb") as f:
+        pickle.dump(q_table, f)
+        
+def load_q_table(filename):
+    with open(filename, "rb") as f:
+        q_table = pickle.load(f)
+    return q_table
+
+def tuplify(w):
+    a = tuple(w[0])
+    b = tuple(w[1])
+    c = tuple(w[2])
+    d = tuple(w[3])
+    f = (a,b,c,d)
+    return f
 
 def simulate():
-    global epsilon, epsilon_decay
+    global epsilon, epsilon_decay, q_table
     for episode in range(MAX_EPISODES):
 
         # Init environment
@@ -21,24 +39,35 @@ def simulate():
                 action = env.action_space.sample()
                 next_state, reward, terminated, truncated, _ = env.step(action) 
                 if(next_state == state):
-                    print("Adriel is a dumb")
                     action = env.action_space.sample()
                     next_state, reward, terminated, truncated, _ = env.step(action)                 
             else:
-                action = np.argmax(q_table[state])
+                # Initialize action with a default value
+                best_action = None
+                best_q_value = float('-inf')  # Initialize with negative infinity
+
+                # Iterate over possible actions
+                for a in range(env.action_space.n):
+                    q_value = q_table.get((tuplify(state), a), 0.0)  # Get Q-value for the current state-action pair
+                    if q_value > best_q_value:
+                        best_q_value = q_value
+                        best_action = a
+
+                # Assign the best action
+                action = best_action
 
             # Do action and get result
             done = terminated or truncated
             total_reward += reward
 
             # Get correspond q value from state, action pair
-            q_value = q_table[state][action]
+            q_value = q_table.get((tuplify(state), action), 0.0)
             # print("newstate: ", next_state)
-            # print("q[newstate]: ", q_table[next_state])
-            best_q = np.max(q_table[next_state])
+            print("q_value: ", q_table.get((tuplify(state), action), 0.0))
+            best_q = max(q_table.get((tuplify(next_state), a), 0.0) for a in range(env.action_space.n))
 
             # Q(state, action) <- (1 - a)Q(state, action) + a(reward + rmaxQ(next state, all actions))
-            q_table[state][action] = (1 - learning_rate) * q_value + learning_rate * (reward + gamma * best_q)
+            q_table[(tuplify(state), action)] = (1 - learning_rate) * q_value + learning_rate * (reward + gamma * best_q)
 
             # Set up for the next iteration
             state = next_state
@@ -49,6 +78,7 @@ def simulate():
             # When episode is done, print reward
             if done or t >= MAX_TRY - 1:
                 print("Episode %d finished after %i time steps with total reward = %f." % (episode, t, total_reward))
+                save_q_table(q_table, 'q_table.pkl')
                 break
 
         # exploring rate decay
@@ -61,14 +91,14 @@ if __name__ == "__main__":
     MAX_EPISODES = 1
     MAX_TRY = 10000
     epsilon = 1
-    epsilon_decay = 0.999
+    epsilon_decay = 0.999 #decay slow because observation space is sparse
     learning_rate = 0.1
     gamma = 0.6
-    # num_box = tuple((env.observation_space.high + np.ones(env.observation_space.shape)).astype(int))
-    # q_table = np.zeros(num_box + (env.action_space.n,))
-    # observation_shape_flat = np.prod(env.observation_space.shape)
     num_actions = 4  # Number of actions
     observation_space_length = 16
-    q_table = np.zeros((observation_space_length * num_actions * 1000))
-    # q_table = np.zeros((observation_shape_flat, env.action_space.n))
+    # q_table = np.zeros((observation_space_length, num_actions))
+    # q_table = load_q_table("q_table.pkl")
+    q_table = {}
     simulate()
+
+
